ff2df40dd2e11901e6fa90addba18343e218e22e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-18 20:42:45 +0100

    m52xq: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-M526B Script version: 3.0.0

---
+++ omni_m52xq.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from m52xq device
+$(call inherit-product, device/samsung/m52xq/device.mk)
+
+PRODUCT_DEVICE := m52xq
+PRODUCT_NAME := omni_m52xq
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M526B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m52xqins-user 11 RP1A.200720.012 M526BXXS4CWL2 release-keys"
+
+BUILD_FINGERPRINT := samsung/m52xqins/m52xq:11/RP1A.200720.012/M526BXXS4CWL2:user/release-keys
